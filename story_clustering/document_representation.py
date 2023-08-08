11f3d335ea2763807c2a9e49f6579cd868c29917
Benjamin Akhras <b@akhras.at>
2023-08-08 17:00:04 +0200

    start fixing linting errors

--- story_clustering/document_representation.py
+++ story_clustering/document_representation.py
@@ -1,9 +1,9 @@
-from collections.abc import Callable
 import math
-from typing import Any
-from .nlp_utils import idf,tfidf
-from typing_extensions import Self
 import json
+from typing import Any
+from .nlp_utils import idf, tfidf
+
+
 class Keyword:
     """
     A class to represent the keyword data type
@@ -14,7 +14,7 @@ class Keyword:
     word: list[str]
         List of words with similar form
     tf: double
-        Term frequency of the word 
+        Term frequency of the word
     df: double
         Document frequency of the word
     documents: set
@@ -23,7 +23,8 @@ class Keyword:
     ------------
     TODO
     """
-    def __init__(self,baseform, words, documents=None, tf=0, df=0) -> None:
+
+    def __init__(self, baseform, words, documents=None, tf=0, df=0):
         self.baseForm = baseform
         self.words = words
         self.tf = tf
@@ -31,34 +32,34 @@ class Keyword:
         if documents != None:
             self.documents = documents
         else:
-            self.documents = set()
-    
-    def increase_tf(self,k) -> None:
+            self.documents = {}
+
+    def increase_tf(self, k):
         self.tf += k
-    
-    def increase_df(self,k) -> None:
+
+    def increase_df(self, k):
         self.df += k
-    
-    def documents(self) -> set():
+
+    def documents(self) -> set:
         return self.documents
-    
+
     def tf(self):
         return self.tf
-    
+
     def df(self):
         return self.df
-    
+
     def reprJSON(self):
-        return {'baseForm': self.baseForm, 'words':self.words, 'tf': self.tf, 'df': self.df, 'documents': list(self.documents) }
-    #def add_document(self, doc_id):
-        
-        
-#class KeywordEncoder(json.JSONEncoder):
+        return {"baseForm": self.baseForm, "words": self.words, "tf": self.tf, "df": self.df, "documents": list(self.documents)}
+
+    # def add_document(self, doc_id):
+
+
+# class KeywordEncoder(json.JSONEncoder):
 #    def default(self, o: Any) -> Any:
 #        return {'baseForm': o.baseForm, 'tf': o.tf, 'df': o.df, 'documents': list(o.documents) }
-        
 
-    
+
 class Document:
     """
     A class to represent a document in a corpus
@@ -81,8 +82,8 @@ class Document:
     content: str
         Document content
     tfitfVecorSizeWithKeygraph: double
-        Document TF-IDF vector size that consider keygraph keywords. For keywords that doesn't included in a keygraph, we don't calculate the keyword's tf. 
-        This is used to calculate the similarity between a keygraph and a document. It is the norm of document vector, where each element in the vector 
+        Document TF-IDF vector size that consider keygraph keywords. For keywords that doesn't included in a keygraph, we don't calculate the keyword's tf.
+        This is used to calculate the similarity between a keygraph and a document. It is the norm of document vector, where each element in the vector
         is the feature (such as tf, tf-idf) of one document keyword.
     tfidfVectorSize: double
         Document TF-IDF vector size.
@@ -90,79 +91,93 @@ class Document:
         Document TF vector's size.
     processed: bool
         Whether this document has been processed.
-        
+
     Methods
     -----------
     TODO
-    
+
     """
-    def __init__(self, doc_id, url = None, publish_date = None, language = None, title = None, content = None, keywords = None):
+
+    def __init__(self, doc_id, url=None, publish_date=None, language=None, title=None, content=None, keywords=None):
         self.doc_id = doc_id
         self.url = url
         self.publishTime = publish_date
         self.language = language
         self.title = title
-        self.segTitle = title.strip().split(' ')
+        self.segTitle = title.strip().split(" ")
         self.content = content
         self.keywords = keywords
         self.tfVectorSize = -1
         self.tfidfVectorSize = -1
         self.tfidfVectorSizeWithKeygraph = -1
-    
-    def containsKeyword(self,kw):
-        if kw in self.keywords:
-            return True
-        return False
+
+    def containsKeyword(self, kw):
+        return kw in self.keywords
+
     # compute document's TF vector size
     # Given document's keywords [w1,..,wn] the vector size of a doc is sqrt(tf(wi)^2)
     def calcTFVectorSize(self):
-        tfVectorSize = 0
-        for k in self.keywords.values():
-            tfVectorSize += math.pow(k.tf,2)
-        
+        tfVectorSize = sum(math.pow(k.tf, 2) for k in self.keywords.values())
         tfVectorSize = math.sqrt(tfVectorSize)
         self.tfVectorSize = tfVectorSize
         return tfVectorSize
-    
+
     def calcTFIDFVectorSize(self, DF, docSize):
-        tfidfVectorSize = 0
-        for k in self.keywords.values():
-            tfidfVectorSize += math.pow(tfidf(k.tf, idf(DF[k.baseForm], docSize)),2)
+        tfidfVectorSize = sum(math.pow(tfidf(k.tf, idf(DF[k.baseForm], docSize)), 2) for k in self.keywords.values())
         tfidfVectorSize = math.sqrt(tfidfVectorSize)
         self.tfidfVectorSize = tfidfVectorSize
-        return 
-    
+        return
+
     @staticmethod
-    def cosineSimilarityByTF(d1: Self, d2 : Self) -> float:
+    def cosineSimilarityByTF(d1, d2) -> float:
         sim = 0
         for k1 in d1.keywords.values():
             if k1.baseForm in d2.keywords:
                 tf1 = k1.tf
                 tf2 = d2.keywords[k1.baseForm].tf
                 sim += tf1 * tf2
-        
+
         if d1.tfVectorSize < 0:
             d1.calcTFVectorSize()
-        
+
         if d2.tfVectorSize < 0:
             d2.calcTFVectorSize()
-        
+
         if d1.tfVectorSize == 0 or d2.tfVectorSize == 0:
             return 0
         else:
             return sim / d1.tfVectorSize / d2.tfVectorSize
-    
+
     def reprJSON(self):
-        return {'doc_id': self.doc_id, 'url': self.url, 'publish_time':self.publishTime, 'language':self.language,
-                'title': self.title, 'content': self.content, 'keywords': [k.reprJSON() for k in self.keywords.values()], 'tfVectorSize': self.tfVectorSize,
-                'tfidfVectorSize': self.tfidfVectorSize, 'tfidfVectorSizeWithKeygraph': self.tfidfVectorSizeWithKeygraph}
-        
+        return {
+            "doc_id": self.doc_id,
+            "url": self.url,
+            "publish_time": self.publishTime,
+            "language": self.language,
+            "title": self.title,
+            "content": self.content,
+            "keywords": [k.reprJSON() for k in self.keywords.values()],
+            "tfVectorSize": self.tfVectorSize,
+            "tfidfVectorSize": self.tfidfVectorSize,
+            "tfidfVectorSizeWithKeygraph": self.tfidfVectorSizeWithKeygraph,
+        }
+
+
 class DocumentEncoder(json.JSONEncoder):
     def default(self, o: Any) -> Any:
-        return {'doc_id': o.doc_id, 'url': o.url, 'publish_time':o.publishTime, 'language':o.language,
-                'title': o.title, 'content': o.content, 'keywords': json.dumps(o.keywords, cls=KeywordEncoder), 'tfVectorSize': o.tfVectorSize,
-                'tfidfVectorSize': o.tfidfVectorSize, 'tfidfVectorSizeWithKeygraph': o.tfidfVectorSizeWithKeygraph}
-    
+        return {
+            "doc_id": o.doc_id,
+            "url": o.url,
+            "publish_time": o.publishTime,
+            "language": o.language,
+            "title": o.title,
+            "content": o.content,
+            "keywords": json.dumps(o.keywords, cls=KeywordEncoder),
+            "tfVectorSize": o.tfVectorSize,
+            "tfidfVectorSize": o.tfidfVectorSize,
+            "tfidfVectorSizeWithKeygraph": o.tfidfVectorSizeWithKeygraph,
+        }
+
 
 class Corpus:
     """
@@ -171,60 +186,46 @@ class Corpus:
     ----------
     docs: dict (doc_id -> Document)
           documents contained in this corpus
-    DF: dict (String -> double) 
+    DF: dict (String -> double)
         Words' DF
-    
+
     Methods:
-    ----------  
+    ----------
     """
-    def __init__(self, docs = None, df = None) -> None:
+
+    def __init__(self, docs=None, df=None) -> None:
         """
         Args:
             docs (_type_, optional): _description_. Defaults to None.
             df (_type_, optional): _description_. Defaults to None.
-        """        
-        self.docs = dict()
-        if df is None:
-            self.DF = dict()
-        else:
-            self.DF = df
+        """
+        self.docs = {}
+        self.DF = {} if df is None else df
         if docs is not None:
-            for doc_id,doc in enumerate(docs):
+            for doc_id, doc in enumerate(docs):
                 self.docs[doc_id] = doc
                 for keyword in doc.keywords().values():
                     if keyword.baseForm() in self.DF:
-                        self.DF[keyword.baseForm()] +=1
+                        self.DF[keyword.baseForm()] += 1
                     else:
                         self.DF[keyword.baseForm()] = 1
-    
 
-    
     def updateDF(self):
-        self.DF = dict()
+        self.DF = {}
         for d in self.docs.values():
             for k in d.keywords.values():
-                if k.baseForm in self.DF:
-                    self.DF[k.baseForm] = self.DF[k.baseForm] + 1
-                else:
-                    self.DF[k.baseForm] = 1
-                    
+                self.DF[k.baseForm] = self.DF[k.baseForm] + 1 if k.baseForm in self.DF else 1
+
     def reprJSON(self):
-        return {'docs': self.docs, 'DF':self.DF}
-        
-                     
+        return {"docs": self.docs, "DF": self.DF}
+
+
 class CorpusEncoder(json.JSONEncoder):
     def default(self, o: Any) -> Any:
-        if hasattr(o,'reprJSON'):
+        if hasattr(o, "reprJSON"):
             return o.reprJSON()
         else:
             return json.JSONEncoder.default(self, o)
 
-
-        
-        
-        
-        #json_docs = { k : json.dumps(v,cls=DocumentEncoder) for k,v in o.docs.items()}
-        #json_df = json.dumps(o.DF)
-        
-        
-        
+        # json_docs = { k : json.dumps(v,cls=DocumentEncoder) for k,v in o.docs.items()}
+        # json_df = json.dumps(o.DF)
--- story_clustering/event_organizer.py
+++ story_clustering/event_organizer.py
@@ -2,48 +2,47 @@ import time
 from .document_representation import Document, Keyword
 from .keywords_organizer import KeywordGraph, KeywordNode, KeywordEdge
 
+
 class Event:
-    def __init__(self) -> None:
+    def __init__(self):
         self.max_id = 1
-        #self.keyGraph = dict()
-        self.docs = dict()
-        self.similarities = dict()
+        # self.keyGraph = dict()
+        self.docs = {}
+        self.similarities = {}
         self.centroid = None
         self.variance = None
         self.summary = None
         self.hotness = None
-        
+
     # get start timestamp of all docs in this event
     # return -1 if no docs in this event
     def getStartTimestamp(self):
         if len(self.docs) == 0:
             return -1
-        
+
         timestamp = time.time()
         for d in self.docs.values():
-            if d.publishTime is not None:
-                if d.publishTime.getTime() < timestamp:
-                    timestamp = d.publishTime.getTime()
+            if d.publishTime is not None and d.publishTime.getTime() < timestamp:
+                timestamp = d.publishTime.getTime()
         return timestamp
 
     # get end timestamp of all docs in this event
-    # return -1 if no doc in this event 
+    # return -1 if no doc in this event
     def getEndTimestamp(self):
         if len(self.docs) == 0:
             return -1
-        
+
         timestamp = -1
         for d in self.docs.values():
-            if d.publishTime is not None:
-                if d.publishTime.getTime()> timestamp:
-                    timestamp = d.publishTime.getTime()
+            if d.publishTime is not None and d.publishTime.getTime() > timestamp:
+                timestamp = d.publishTime.getTime()
         return timestamp
-    
+
     # calculate the centroid document of this document cluster
     # centroid is the concatenation of all docs in this event
     def calcCentroid(self):
-        self.centroid = Document('-1')
-        timestamp = float('inf')
+        self.centroid = Document("-1")
+        timestamp = float("inf")
         for d in self.docs.values():
             if d.publishTime.getTime() < timestamp:
                 timestamp = d.publishTime.getTime()
@@ -53,27 +52,20 @@ class Event:
                     kk.tf += k.tf
                     kk.df += k.df
                 else:
-                    self.centroid[k.baseForm] = Keyword(k.baseForm, k.word, k.tf,k.df)
-        
+                    self.centroid[k.baseForm] = Keyword(k.baseForm, k.word, k.tf, k.df)
+
         self.centroid.calcTFVectorSize()
         self.centroid.publishTime = timestamp
-    
+
     def refineKeyGraph(self):
-        toRemove = list()
+        toRemove = []
         for key in self.keyGraph.graphNodes:
             keywordNode = self.keyGraph.graphNodes[key]
             keyword = keywordNode.keyword.baseForm
-            exist = False
-            
-            for d in self.docs.values():
-                if d.containsKeyword(keyword):
-                    exist = True
-                    break
-            
+            exist = any(d.containsKeyword(keyword) for d in self.docs.values())
             if not exist:
                 toRemove.append(keyword)
-        
+
         for kw in toRemove:
             self.keyGraph.graphNodes.pop(kw)
-            #KeywordGraph.removeNode(self.keyGraph,kw)
-    
+            # KeywordGraph.removeNode(self.keyGraph,kw)
