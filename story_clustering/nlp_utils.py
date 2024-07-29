b04a9e5599b193668362ace5eef90db545069e17
Benjamin Akhras <b@akhras.at>
2024-07-29 20:26:08 +0200

    update CI/CD

--- story_clustering/nlp_utils.py
+++ story_clustering/nlp_utils.py
@@ -6,6 +6,7 @@ from story_clustering import stopwords_list
 
 POLYFUZZ_THRESHOLD = 3
 
+
 # calculate word's inverse document frequency
 def idf(df: float, size: int):
     return math.log(size / (df + 1)) / math.log(2)
@@ -13,6 +14,7 @@ def idf(df: float, size: int):
 
 def replace_umlauts_with_digraphs(s: str) -> str:
     return s.lower().replace("ä", "ae").replace("ö", "oe").replace("ü", "ue").replace("ß", "ss")
+
 
 # calculate word's tf-idf
 def tfidf(tf, idf):
@@ -35,7 +37,7 @@ def compute_tf(baseForm, text):
     tokenized_text = tokanize_text(text)
     try:
         model.match(tokenized_text, [baseForm]).group(link_min_similarity=0.75)
-    except:
+    except Exception:
         return 1
     df = model.get_matches()
     if len(df) == 0:
@@ -48,21 +50,19 @@ def compute_tf(baseForm, text):
     tf = max(values["Similarity"].sum(), 1)
     return tf / n_words
 
-def find_keywords_matches(keywords_lst1:list[str],keywords_lst2:list[str]) -> int:
+
+def find_keywords_matches(keywords_lst1: list[str], keywords_lst2: list[str]) -> int:
     model = PolyFuzz(TFIDF(model_id="TF-IDF-Sklearn", clean_string=False, n_gram_range=(3, 3)))
 
-    if len(keywords_lst1) == 0 or len(keywords_lst2) == 0:
+    if not keywords_lst1 or not keywords_lst2:
         return 0
     try:
         model.match(keywords_lst1, keywords_lst2).group(link_min_similarity=0.75)
-    except:
+    except Exception:
         return 0
     df = model.get_matches()
     if len(df) == 0:
         return 0
     values = df[(df["Group"].notnull()) & (df["Similarity"] >= 0.65)]
     values.loc[:, ("Similarity")] = 1
-    shared_keywords = max(values["Similarity"].sum(), 1)
-    #if len(values) == 0:
-    #    return 0
-    return shared_keywords #/ min(len(keywords_lst1),len(keywords_lst2))
+    return max(values["Similarity"].sum(), 1)
