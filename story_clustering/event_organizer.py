ecffb74d7598df39193c3bd5fe1a27be8a74858e
llinauer <l.linauer@gmx.at>
2025-07-07 15:09:07 +0200

    Lint and simplify Event class

--- story_clustering/event_organizer.py
+++ story_clustering/event_organizer.py
@@ -1,47 +1,44 @@
 from .document_representation import Document, Keyword
 from .keywords_organizer import KeywordGraph
+from datetime import datetime
 
 
 class Event:
     def __init__(self, key_graph: KeywordGraph):
-        self.max_id = 1
         self.key_graph = key_graph
-        self.docs = {}
+        self.max_id: int = 1
+        self.docs: dict[str, Document] = {}
         self.similarities = {}
-        self.centroid = None
-        self.variance = None
-        self.summary = None
-        self.hotness = None
+        self.centroid: Document = Document("-1")
 
-    # calculate the centroid document of this document cluster
-    # centroid is the concatenation of all docs in this event
     def calc_centroid(self):
-        self.centroid = Document("-1")
-        timestamp = float("inf")
+        # calculate the centroid document of this document cluster
+        # centroid is the concatenation of all docs in this event
+
+        timestamp = datetime.max
         for doc in self.docs.values():
-            if doc.publish_time.getTime() < timestamp:
-                timestamp = doc.publish_time.getTime()
+            if doc.publish_time < timestamp:
+                timestamp = doc.publish_time
             for k in doc.keywords.values():
-                if k.baseform in self.centroid.keywords and self.centroid.keywords:
+                if self.centroid.keywords and k.baseform in self.centroid.keywords:
                     kk = self.centroid.keywords[k.baseform]
                     kk.tf += k.tf
                     kk.df += k.df
                 else:
-                    self.centroid.set_keyword(Keyword(k.baseform, k.tf, k.df))
+                    self.centroid.set_keyword(Keyword(k.baseform, set(), k.tf, k.df))
 
         self.centroid.calc_tf_vector_size()
         self.centroid.publish_time = timestamp
 
     def refine_key_graph(self):
+        # remove all of the events key_graphs keywords that do not appear in any of the events docs
+
         to_remove = []
         if not self.key_graph:
             return
-        for key in self.key_graph.graph_nodes:
-            keyword_node = self.key_graph.graph_nodes[key]
-            keyword = keyword_node.keyword.baseform
-            exist = any(d.contains_keyword(keyword) for d in self.docs.values())
-            if not exist:
-                to_remove.append(keyword)
 
+        to_remove.extend(
+            keyword for keyword in self.key_graph.graph_nodes.keys() if not any(d.contains_keyword(keyword) for d in self.docs.values())
+        )
         for keyword in to_remove:
             self.key_graph.graph_nodes.pop(keyword)
