fdacf95dbac6b4136225a3c6fd1a9e8d8ba22957
llinauer <l.linauer@gmx.at>
2025-11-04 12:04:00 +0100

    Move to taranis-base-bot, remove now-obsolete files

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
-from story_clustering import create_app
+from story_clustering.config import Config
+from taranis_base_bot import create_app
 
-app = create_app()
+app = create_app(Config.PACKAGE_NAME, Config)
