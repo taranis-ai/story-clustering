7233f7abab352fb8fc7e40c78bb5afc18ab23173
llinauer <l.linauer@gmx.at>
2025-11-04 09:13:24 +0100

    Update build_container.sh with MODEL and REPO_NAME env vars

--- build_container.sh
+++ build_container.sh
@@ -2,16 +2,24 @@
 
 set -eou pipefail
 
+if [ ! -d .git ]; then
+    echo "This script must be run from the root of a git repository"
+    exit 1
+fi
+
 cd $(git rev-parse --show-toplevel)
 
 GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY_OWNER:-"ghcr.io/taranis-ai"}
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
 
-echo "Building containers for branch ${CURRENT_BRANCH} on ${GITHUB_REPOSITORY_OWNER}"
+MODEL=${MODEL:-"tfidf"}
+REPO_NAME=$(grep 'url =' .git/config | sed -E 's/.*[:\/]([^\/]+)\.git/\1/')
+
+echo "Building containers for branch ${CURRENT_BRANCH} with model ${MODEL} on ${GITHUB_REPOSITORY_OWNER}"
 
 docker buildx build --file Containerfile \
   --build-arg GITHUB_REPOSITORY_OWNER="${GITHUB_REPOSITORY_OWNER}" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/taranis-story-clustering-bot:latest" \
-  --tag "${GITHUB_REPOSITORY_OWNER}/taranis-story-clustering-bot:${CURRENT_BRANCH}" \
+  --build-arg MODEL="${MODEL}" \
+  --tag "${GITHUB_REPOSITORY_OWNER}/${REPO_NAME}:latest" \
+  --tag "${GITHUB_REPOSITORY_OWNER}/${REPO_NAME}:${MODEL}" \
   --load .
-
