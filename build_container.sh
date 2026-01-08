8a573a13a02a50dc58ca2cac5ba662fc5e06751b
Lukas Linauer <l.linauer@gmx.at>
2026-01-08 12:31:18 +0100

    Sync with bot_template (#22)
    * sync with bot_template
    
    * Update README.md
    
    * Set correct repo name in smoke_test.yml
    
    * Start working on openapi3_1.yml
    
    * Update openapi3_1.yml
    
    * Add compose file; include it in release
    
    * Rename repo_name -> image_name in accordance with github action
    
    * Make port in release an env var
    
    ---------
    
    Co-authored-by: llinauer <llinauer@users.noreply.github.com>

--- build_container.sh
+++ build_container.sh
@@ -7,8 +7,13 @@ cd $(git rev-parse --show-toplevel)
 GITHUB_REPOSITORY_OWNER=${GITHUB_REPOSITORY_OWNER:-"ghcr.io/taranis-ai"}
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9_.-]/_/g')
 
+<<<<<<< before updating
 MODEL=${MODEL:-"louvain"}
 REPO_NAME=$(basename -s .git "$(git config --get remote.origin.url)")
+=======
+MODEL=${MODEL:-"<default_model>"}
+REPO_NAME=taranis-$(basename -s .git "$(git config --get remote.origin.url)")
+>>>>>>> after updating
 
 echo "Building containers for branch ${CURRENT_BRANCH} with model ${MODEL} on ${GITHUB_REPOSITORY_OWNER}"
 
