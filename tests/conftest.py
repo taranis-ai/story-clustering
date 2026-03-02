273c14361bade9f9c15ce2d9f05157bb6f1e9484
llinauer <l.linauer@gmx.at>
2026-03-02 06:55:06 +0100

    Download nltk stopwords during testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+import nltk
 
 
 @pytest.fixture(scope="session")
@@ -12,3 +13,11 @@ def story_list():
 def clustered_stories_list():
     with open("./clustered_stories_list.json", "r") as f:
         yield json.load(f)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def ensure_nltk_stopwords():
+    try:
+        nltk.data.find("corpora/stopwords")
+    except LookupError:
+        nltk.download("stopwords", quiet=True)
