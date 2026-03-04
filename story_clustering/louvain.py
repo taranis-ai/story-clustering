7f6b2c3eb8c85c74603a13bc5b6bf68113d465f8
llinauer <l.linauer@gmx.at>
2026-03-04 15:44:36 +0100

    Make predict func in louvain.py async

--- story_clustering/louvain.py
+++ story_clustering/louvain.py
@@ -1,3 +1,4 @@
+import asyncio
 from story_clustering.clustering import Cluster
 from typing import Any
 
@@ -8,13 +9,13 @@ class Louvain:
     def __init__(self):
         self.model = Cluster()
 
-    def predict(self, stories: list[dict]) -> dict[str, Any]:
+    async def predict(self, stories: list[dict]) -> dict[str, Any]:
         if all(len(story["news_items"]) == 1 for story in stories):
-            clustering_results = self.model.initial_clustering(stories)
+            clustering_results = await asyncio.to_thread(self.model.initial_clustering, stories)
             msg = f"Initial Clustering done with: {len(stories)} news items"
             return {"cluster_ids": clustering_results, "message": msg}
 
         already_clustered, to_cluster = self.model.separate_stories(stories)
-        clustering_results = self.model.incremental_clustering_v2(to_cluster, already_clustered)
+        clustering_results = await asyncio.to_thread(self.model.incremental_clustering_v2, to_cluster, already_clustered)
         msg = f"incremental Clustering done with: {len(stories)} news items"
         return {"cluster_ids": clustering_results, "message": msg}
